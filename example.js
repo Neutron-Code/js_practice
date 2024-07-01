5599a98cdc2561e9261d9e70f5b4f322a3b5e76e
Harish Puri <githubuser.me@gmail.com>
2024-07-01 14:33:25 +0530

    Wrote an unefficient code to find the prime numbers

--- example.js
+++ example.js
@@ -1,3 +1,36 @@
-function isPrime(){
-    
+const maxNum = 100000000;
+const log = document.querySelector('#xhr');
+
+function isPrime(number){
+    for(let i = 2; i < Math.sqrt(number); i++){
+        if(number % i === 0){
+            return false
+        }
+    }
+
+    return (number > 1)
 }
+
+const randomNumber = (limit) => Math.floor(Math.random() * limit);
+
+function getPrime(){
+    const primes = [];
+    while(primes.length < 10000000){
+        const randomNumberGenerated = randomNumber(maxNum);
+        if(isPrime(randomNumberGenerated)){
+            primes.push(randomNumberGenerated);
+        }
+    }
+
+    return primes;
+}
+
+const sortedArray = (array) => array.sort((a, b) => a - b) 
+const primesArray = getPrime();
+const sortedPrimesArray = sortedArray(primesArray);
+
+log.addEventListener('click', () => {
+    console.log('I am Clicked and I am alive')
+})
+
+console.log(sortedPrimesArray);
