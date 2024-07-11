6246e03c742a996cb8851360f0a62c1fc9db8f6e
Harish Puri <githubuser.me@gmail.com>
2024-07-11 08:01:25 +0530

    Learned how to use the async await

--- example.js
+++ example.js
@@ -1,13 +1,31 @@
-// This is the code for the Apply Method used for the function
+const fetchThree = fetch('https://jsonplaceholder.typicode.com/todos/3');
 
-function greetMe(age){
-    return `My fullname is ${this.firstName} ${this.lastName} and my age is ${age}`
+async function processPromise(){
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/2');
+        if(!response.ok) throw new Error(`Http Error: ${response.status}`);
+
+        const data = await response.json();
+        console.log(data);
+        return {
+            message: 'This is the message from another async funciton',
+            port: 90937,
+            urlId: 'user.web/malware.com',
+            refId: 'x0eRf574xd'
+        }
+    }
+    
+    catch(err){
+        console.error(err)
+    }
+    
+    finally{
+        console.log('Request Finished')
+    }
 }
 
-const person = {
-    firstName: 'Harish',
-    lastName: 'puri'
-}
-const greet = greetMe.apply(person, [17]);
-
-console.log(greet);
+document.getElementById('xhr').addEventListener('click', async () => {
+    const resultOfPromise = processPromise();
+    const res = await resultOfPromise;
+    console.log(res); 
+})
